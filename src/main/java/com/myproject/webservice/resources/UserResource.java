11b2d82931b51473b9d36166ebfa7cf5b02b0337
Erick Mendes <dev.erickmendes126@gmail.com>
2025-07-18 14:50:14 -0300

    Service layer, component registration

--- src/main/java/com/myproject/webservice/resources/UserResource.java
+++ src/main/java/com/myproject/webservice/resources/UserResource.java
@@ -1,18 +1,25 @@
 package com.myproject.webservice.resources;
 
 import com.myproject.webservice.entities.User;
+import com.myproject.webservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> find(){
-        User user = new User(1L, "Erick Mendes", "erickmendes@gmail.com", "98970188273", "123");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/myproject/webservice/services/UserService.java
@@ -0,0 +1,20 @@
+package com.myproject.webservice.services;
+
+import com.myproject.webservice.entities.User;
+import com.myproject.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        List<User> list = repository.findAll();
+        return list;
+    }
+}
